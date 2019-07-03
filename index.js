b6e4e9ae1328f585c4ca57ab1215b1dc76915e67
Jeroen Bruinsma <jeroenbruinsma@gmail.com>
2019-07-03 15:21:47 +0200

    added cors config

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let port;
 require("./db.js")
 const advertisement = require('./advertisement/router')
 //const tokens = require('./tokens/middleware')
-
+const cors = require('cors')
 
 if(!process.env.PORT){
      port = 40004
@@ -18,6 +18,7 @@ console.log("Api server on port", port)
 app.get('/', (req, res) => res.send('Server running'))
 
 app.listen(port, () => `Listening on port ${port}`)
+app.use(cors())
 app.use(jsonParser)
 app.use(advertisement)
 // app.use(tokens)
